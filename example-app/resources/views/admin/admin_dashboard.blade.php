cf6839d1c7568d6c6d379740d38a0b8710a3a0b8
annk1995 <kabuikariuki052@gmail.com>
2024-01-24 17:24:00 +0300

    profile data

--- example-app/resources/views/admin/admin_dashboard.blade.php
+++ example-app/resources/views/admin/admin_dashboard.blade.php
@@ -23,6 +23,7 @@ License: For each use you must have a valid license purchased only from above li
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
   <!-- End fonts -->
 
 	<!-- core:css -->
@@ -84,7 +85,32 @@ License: For each use you must have a valid license purchased only from above li
 
 	<!-- Custom js for this page -->
   <script src="{{ asset('backend/assets/js/dashboard-dark.js') }}"></script>
-	<!-- End custom js for this page -->
 
+
+	<!-- End custom js for this page -->
+ <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+<script>
+ @if(Session::has('message'))
+ var type = "{{ Session::get('alert-type','info') }}"
+ switch(type){
+    case 'info':
+    toastr.info(" {{ Session::get('message') }} ");
+    break;
+
+    case 'success':
+    toastr.success(" {{ Session::get('message') }} ");
+    break;
+
+    case 'warning':
+    toastr.warning(" {{ Session::get('message') }} ");
+    break;
+
+    case 'error':
+    toastr.error(" {{ Session::get('message') }} ");
+    break;
+ }
+ @endif
+</script>
 </body>
 </html>
--- example-app/resources/views/admin/body/header.blade.php
+++ example-app/resources/views/admin/body/header.blade.php
@@ -182,6 +182,10 @@
                     </div>
                 </div>
             </li>
+           @php
+            $id=Auth::user()->id;
+            $data =App\Models\User::find($id);
+            @endphp
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <img class="wd-30 ht-30 rounded-circle" src="https://via.placeholder.com/30x30" alt="profile">
@@ -189,7 +193,7 @@
                 <div class="dropdown-menu p-0" aria-labelledby="profileDropdown">
                     <div class="d-flex flex-column align-items-center border-bottom px-5 py-3">
                         <div class="mb-3">
-                            <img class="wd-80 ht-80 rounded-circle" src="https://via.placeholder.com/80x80" alt="">
+                            <img class="wd-80 ht-80 rounded-circle" src="{{ (!empty($profileData->photo)) ? url('upload/admin_images/'.$profileData->photo) : url('upload/no_image.jpg') }}"  alt="">
                         </div>
                         <div class="text-center">
                             <p class="tx-16 fw-bolder">kabui kariuki</p>
@@ -200,7 +204,7 @@
       <li class="dropdown-item py-2">
         <a href="{{ route('admin.profile') }}" class="text-body ms-0">
           <i class="me-2 icon-md" data-feather="user"></i>
-          <span>Profile</span> 
+          <span>Profile</span>
         </a>
       </li>
       <li class="dropdown-item py-2">
